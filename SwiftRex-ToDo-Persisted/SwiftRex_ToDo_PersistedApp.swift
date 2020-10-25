4a831cca7c9324e6ce59011cf2234a1fd085cace
Adrian Ward <adrian@awardvetsci.com>
2020-10-25 14:19:42 +0000

    Initial Commit

---
+++ SwiftRex-ToDo-Persisted/SwiftRex_ToDo_PersistedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRex_ToDo_PersistedApp.swift
+//  SwiftRex-ToDo-Persisted
+//
+//  Created by Adrian Ward on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRex_ToDo_PersistedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftRex-ToDo-PersistedTests/SwiftRex_ToDo_PersistedTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftRex_ToDo_PersistedTests.swift
+//  SwiftRex-ToDo-PersistedTests
+//
+//  Created by Adrian Ward on 25/10/2020.
+//
+
+import XCTest
+@testable import SwiftRex_ToDo_Persisted
+
+class SwiftRex_ToDo_PersistedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
